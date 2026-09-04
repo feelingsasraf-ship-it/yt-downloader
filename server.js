67c2e85f8de4a768cb218c1da58e5e9127b64578
feelingsasraf-ship-it <feelingsasraf@gmail.com>
2026-09-04 22:43:58 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -36,3 +36,7 @@ app.post('/download', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
